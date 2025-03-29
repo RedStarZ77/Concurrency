00b5a97894701a9950e4f40c0d0c45729a76344a
RedStarZ77 <redstarz2002@mail.ru>
2025-03-29 19:51:18 +0300

    Thread run

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,32 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start");
-        MyRunnable thread = new MyRunnable();
-        Thread thread1 = new Thread(thread);
-        thread1.start();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 1000; i++) {
+                        if (Thread.currentThread().isInterrupted()) {
+                            break;
+                        }
+                        System.out.print(i);
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         for (int i = 0; i < 1000; i++) {
+            if (i == 100) {
+                thread.interrupt();
+            }
             System.out.print("M");
         }
         System.out.println("\nFinish");
