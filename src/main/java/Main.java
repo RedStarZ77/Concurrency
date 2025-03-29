3898a8fe81468280c72ad096c3c5f8327a1fbef5
RedStarZ77 <redstarz2002@mail.ru>
2025-03-29 20:40:09 +0300

    Thread with timer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    private static List<String> list;
     public static void main(String[] args) {
-        System.out.println("Start");
-        Thread thread = new Thread(new Runnable() {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                list = new ArrayList<>();
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    for (int i = 0; i < 1000; i++) {
-                        if (Thread.currentThread().isInterrupted()) {
-                            break;
-                        }
-                        System.out.print(i);
-                        Thread.sleep(1000);
-                    }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                    thread1.join();
+                } catch (InterruptedException e){
+                    System.out.println("Error");
                 }
+                System.out.println(list.size());
             }
         });
-        thread.start();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        for (int i = 0; i < 1000; i++) {
-            if (i == 100) {
-                thread.interrupt();
-            }
-            System.out.print("M");
-        }
-        System.out.println("\nFinish");
+        thread1.start();
+        thread2.start();
+        System.out.println(System.currentTimeMillis());
     }
 }
