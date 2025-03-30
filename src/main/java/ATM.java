91467a7fd298c1d2e691e3d10c1768c412b0e69c
RedStarZ77 <redstarz2002@mail.ru>
2025-03-30 15:36:42 +0300

    ATM with 4 threads synchronized by monitor

--- src/main/java/ATM.java
+++ src/main/java/ATM.java
@@ -1,18 +1,21 @@
 public class ATM {
-    int value = 1000;
+    private int value = 1000;
+    Object monitor = new Object();
 
-    public synchronized void cashDec(String name, int cash) {
-        System.out.println(name + " подошёл к банкомату");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        if (value >= cash) {
-            value = value - cash;
-            System.out.println(name + " вывел " + cash + " рублей. В банкомате осталось " + value + " рублей");
-        } else {
-            System.out.println("В банкомате недостаточно средств для " + name);
+    public void cashDec(String name, int cash) {
+        synchronized (monitor) {
+            System.out.println(name + " подошёл к банкомату");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (value >= cash) {
+                value = value - cash;
+                System.out.println(name + " вывел " + cash + " рублей. В банкомате осталось " + value + " рублей");
+            } else {
+                System.out.println("В банкомате недостаточно средств для " + name);
+            }
         }
     }
 }
