764f8e3229e0a93ca9d694968704af529669df42
RedStarZ77 <redstarz2002@mail.ru>
2025-03-30 15:15:56 +0300

    4 threads try

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,37 +4,56 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        startTimer();
-        new Thread(new Runnable() {
+        Counter counter = new Counter();
+        long before = System.currentTimeMillis();
+        int barrier = 100_000_000;
+        Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                Concurrency.withConcurrency();
-            }
-        }).start();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Concurrency.withoutConcurrency();
-            }
-        }).start();
-    }
-
-    private static void startTimer(){
-        Thread timer = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int seconds = 0;
-                while (true){
-                    System.out.println(seconds++);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+                for (int i = 0; i < barrier; i++) {
+                    counter.inc();
                 }
             }
         });
-        timer.setDaemon(true);
-        timer.start();
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < barrier; i++) {
+                    counter.dec();
+                }
+            }
+        });
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < barrier; i++) {
+                    counter.inc2();
+                }
+            }
+        });
+        Thread thread4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < barrier; i++) {
+                    counter.dec2();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+            thread4.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(counter.getValue());
+        System.out.println(counter.getValue2());
+        long after = System.currentTimeMillis();
+        System.out.println(after-before);
     }
 }
