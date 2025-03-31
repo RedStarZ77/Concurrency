fc9413d1b01876b8498434b04bce2f8b72679130
RedStarZ77 <redstarz2002@mail.ru>
2025-03-31 11:58:54 +0300

    ExecutorService practice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,36 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
     public static void main(String[] args) {
-        ATM atm = new ATM();
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                atm.cashDec("Серёга", 500);
-            }
-        });
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                atm.cashDec("Коля", 250);
-            }
-        });
-        Thread thread3 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                atm.cashDec("Костя", 500);
-            }
-        });
-        Thread thread4 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                atm.cashDec("Маша", 250);
-            }
-        });
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        CountDownLatch countDownLatch = new CountDownLatch(10);
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Start - " + index);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Finish - " + index);
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        executorService.shutdown();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("All threads are terminated!");
     }
 }
