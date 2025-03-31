bfd9b6995071c29d9dff212a6dfa71544bcd82d8
RedStarZ77 <redstarz2002@mail.ru>
2025-03-31 12:27:13 +0300

    ExecutorService practice 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -7,30 +7,62 @@ import java.util.concurrent.Executors;
 public class Main {
 
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
-        CountDownLatch countDownLatch = new CountDownLatch(10);
-        for (int i = 0; i < 10; i++) {
-            final int index = i;
-            executorService.execute(new Runnable() {
-                @Override
-                public void run() {
-                    System.out.println("Start - " + index);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
+        long before = System.currentTimeMillis();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                long result = 0;
+                for (int i = 0; i <= 1_000_000; i++) {
+                    if (i % 2 == 0) {
+                        result += i;
                     }
-                    System.out.println("Finish - " + index);
-                    countDownLatch.countDown();
                 }
-            });
-        }
+                System.out.println("Сумма всех чётных чисел до миллиона: " + result);
+                countDownLatch.countDown();
+            }
+        });
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                long result = 0;
+                for (int i = 0; i <= 1_000_000; i++) {
+                    if (i % 7 == 0) {
+                        result += i;
+                    }
+                }
+                System.out.println("Сумма всех чисел, которые делятся на 7 без остатка до миллиона: " + result);
+                countDownLatch.countDown();
+            }
+        });
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                Random random = new Random();
+                int randomNumber;
+                ArrayList<Integer> array = new ArrayList<>(100);
+                for (int i = 0; i < 100; i++) {
+                    randomNumber = random.nextInt(1000);
+                    array.add(randomNumber);
+                }
+                long result = 0;
+                for (Integer arr : array) {
+                    if (arr % 2 == 0) {
+                        result += arr;
+                    }
+                }
+                System.out.println("Заполнив коллекцию 1000 элементами и получив сумму чётных чисел в ней, результат: " + result);
+                countDownLatch.countDown();
+            }
+        });
         executorService.shutdown();
         try {
             countDownLatch.await();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("All threads are terminated!");
+        long after = System.currentTimeMillis();
+        System.out.println("Времени было потрачено: " + (after - before));
     }
 }
